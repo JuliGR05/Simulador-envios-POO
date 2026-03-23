5a2080b585bd981b4122e9588d0d39f169400350
Juli <julianita.gutierrez2005@gmail.com>
2026-03-23 18:08:54 -0500

    Corrección clase Camión

--- propoo/Camion.java
+++ propoo/Camion.java
@@ -1,7 +1,7 @@
 package propoo;
 public class Camion extends Terrestre {
     public Camion() { super("Camión de carga", 55.0, 8000.0, 0.5); }
-    @Override public boolean validarDistancia(double d) { return d >= 30; }
-    @Override public String getMensajeErrorDistancia() { return "Mínimo 30km para rutas intermunicipales."; }
+    @Override public boolean validarDistancia(double d) { return d >= 0; }
+    @Override public String getMensajeErrorDistancia() { return ""; }
     @Override public double calcularCosto(double d, double p) { return 45000.0 + (d * p * getCostoFactor()); }
 }
